9841ed16dfe11e091916f80065dcf700fd17e307
Aniruddha Sahasrabuddhe <anirus@ranjeets-air.home>
2014-10-22 18:19:56 -0400

    added google ads on iOS

--- ios/src/com/udaan/sugarpatrol/IOSLauncher.java
+++ ios/src/com/udaan/sugarpatrol/IOSLauncher.java
@@ -1,12 +1,24 @@
 package com.udaan.sugarpatrol;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.coregraphics.CGSize;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIViewController;
+import org.robovm.bindings.admob.GADAdSize;
+import org.robovm.bindings.admob.GADBannerView;
+import org.robovm.bindings.admob.GADRequest;
 
 public class IOSLauncher extends IOSApplication.Delegate implements IActivityRequestHandler{
+    private static final String AD_UNIT_ID = "ca-app-pub-8996795250788622/6788677098";
+
+    private UIViewController rootViewController;
+    private GADBannerView bannerView;
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
@@ -20,12 +32,52 @@ public class IOSLauncher extends IOSApplication.Delegate implements IActivityReq
     }
 
     @Override
-    public void showAds(boolean show) {
+    public void showShop() {
 
     }
 
-    @Override
-    public void showShop() {
+    /**
+     * Creates a new admob ad
+     */
+    private void createAd() {
+        final CGSize screenSize = UIScreen.getMainScreen().getBounds().size();
+        double screenWidth = screenSize.width();
+        double screenHeight = screenSize.height();
 
+        rootViewController = UIApplication.getSharedApplication().getKeyWindow().getRootViewController();
+        bannerView = new GADBannerView(GADAdSize.banner());
+        bannerView.setAdUnitID(AD_UNIT_ID);
+
+        final CGSize adSize = bannerView.getBounds().size();
+        double adWidth = adSize.width();
+        double adHeight = adSize.height();
+        float bannerWidth = (float) screenWidth;
+        float bannerHeight = (float) (bannerWidth / adWidth * adHeight);
+
+        rootViewController.getView().addSubview(bannerView);
+        bannerView.setFrame(new CGRect((screenWidth / 2) - adWidth / 2, screenHeight - bannerHeight, bannerWidth, bannerHeight));
+        bannerView.setRootViewController(rootViewController);
+
+        GADRequest request = GADRequest.create();
+
+        bannerView.loadRequest(request);
+
+        this.showAds(true);
+    }
+
+    @Override
+    public boolean didFinishLaunching (UIApplication application, UIApplicationLaunchOptions launchOptions) {
+        boolean didFinish = super.didFinishLaunching(application, launchOptions);
+
+        this.createAd();
+
+        return didFinish;
+    }
+
+    @Override
+    public void showAds(boolean show) {
+        if (bannerView != null) {
+            bannerView.setHidden(!show);
+        }
     }
 }
